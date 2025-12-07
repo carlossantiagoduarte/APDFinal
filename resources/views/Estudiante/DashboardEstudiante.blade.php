3e90be6aedeb0b061da8de9f007a5b8b671d45d7
patholzin <patholzin@gmail.com>
2025-12-07 17:09:09 -0600

    Update DashboardEstudiante.blade.php

--- resources/views/Estudiante/DashboardEstudiante.blade.php
+++ resources/views/Estudiante/DashboardEstudiante.blade.php
@@ -48,9 +48,9 @@
             </div>
 
             <div id="user-menu" class="dropdown">
-                <a href="{{ route('dashboard.estudiante') }}">
+                <a  style="background-color: #f0f0f0;">
                     <svg viewBox="0 0 24 24" fill="none" stroke="#111" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9.5L12 3l9 6.5V21H3z" /></svg>
-                    Inicio
+                    Inicio (Estudiante)
                 </a>
                 
                 {{-- RUTA CORREGIDA: editarperfil -> profile.edit --}}
