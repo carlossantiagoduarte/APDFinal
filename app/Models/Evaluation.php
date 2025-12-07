6582811486508119c72492d0548e36d8281d8e09
Nyx <blum7490@gmail.com>
2025-12-07 08:05:13 -0600

    Update Evaluation.php

--- app/Models/Evaluation.php
+++ app/Models/Evaluation.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Evaluation extends Model
 {
-    //
+    use HasFactory;
+
+    // ESTO ES LO QUE TE FALTA:
+    protected $fillable = [
+        'team_id',
+        'user_id',
+        'score',
+        'feedback',
+    ];
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function judge()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
